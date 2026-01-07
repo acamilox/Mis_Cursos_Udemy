f88dcb9f535f56be7bf505ca8f521aadc6a393f8
Camilo <acamicamacho@gmail.com>
2026-01-06 19:40:55 -0500

    estructura del máster de Java y ejercicios

---
+++ 3. Máster Completo en Java de cero a experto/FASE 1 - FUNDAMENTOS DE JAVA & SPRING BOOT/1. Máster Completo en Java de cero a experto/Java Class/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+
+    public static String nombre; // Make 'nombre' static
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+    }
+}
